fc283cfa6ed49e0c7ad1e13c21957797c9ab1dda
Gustavo7327 <gustavosilva712493@gmail.com>
2023-10-23 21:08:06 -0300

    finished start controller

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -9,9 +15,27 @@ public class Main extends Application{
 
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        
-        
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamestart.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Pong Game");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });    
+    } 
+
+    private void logout(Stage stage){
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Are you sure you want to leave?");
+        if(alert.showAndWait().get() == ButtonType.OK){
+            stage.close();
+        }
     }
-    
 }
--- StartPaneController.java
+++ StartPaneController.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class StartPaneController {
 
@@ -21,19 +28,53 @@ public class StartPaneController {
     @FXML
     private Text titlegametext;
 
+    private static boolean SINGLEPLAYER;
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
+
     @FXML
     void newOnePlayer(ActionEvent event) {
-
+        SINGLEPLAYER = true;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     void newTwoPlayer(ActionEvent event) {
-
+        SINGLEPLAYER = false;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     void settings(ActionEvent event) {
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamesettings.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
