6c3eb0cccbd22127ad166a5b24f004cbdccf253e
Gustavo7327 <gustavosilva712493@gmail.com>
2023-10-28 17:33:56 -0300

    adding transitions between scenes

--- GamePanelController.java
+++ GamePanelController.java
@@ -1,10 +1,16 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-public class GamePanelController {
+public class GamePanelController implements Initializable{
 
     @FXML
     private Circle ball;
@@ -23,5 +29,18 @@ public class GamePanelController {
 
     @FXML
     private Rectangle redrectangle;
+
+    private FadeTransition fadeTransition;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gamepane.setOpacity(0);
+        fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(gamepane);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+    }
 
 }
--- StartPaneController.java
+++ StartPaneController.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,8 +15,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.scene.Node;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class StartPaneController {
+public class StartPaneController implements Initializable{
 
     @FXML
     private Button but1player;
@@ -32,49 +38,97 @@ public class StartPaneController {
     private Parent root;
     private Scene scene;
     private Stage stage;
+    private FadeTransition fadeTransition;
 
     @FXML
     void newOnePlayer(ActionEvent event) {
         SINGLEPLAYER = true;
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        scene = new Scene(root);
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(startpane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                scene = new Scene(root);
+                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            }    
+        });
+        fadeTransition.play();
     }
 
     @FXML
     void newTwoPlayer(ActionEvent event) {
         SINGLEPLAYER = false;
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        scene = new Scene(root);
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(startpane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("gamepane.fxml"));
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                scene = new Scene(root);
+                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            }    
+        });
+        fadeTransition.play();
     }
 
     @FXML
     void settings(ActionEvent event) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gamesettings.fxml"));
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        scene = new Scene(root);
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(startpane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("gamesettings.fxml"));
+                try {
+                    root = loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                scene = new Scene(root);
+                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            }
+        });
+        fadeTransition.play();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        startpane.setOpacity(0);
+        fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(startpane);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
     }
 
 }
