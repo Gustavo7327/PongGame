ff50efd3d4e6c9b11004ff6d2a7ee8a75e8f5ff4
Gustavo7327 <gustavosilva712493@gmail.com>
2023-10-29 18:09:03 -0300

    adding collision detection

--- GamePanelController.java
+++ GamePanelController.java
@@ -1,9 +1,12 @@
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
+import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -31,6 +34,12 @@ public class GamePanelController implements Initializable{
     private Rectangle redrectangle;
 
     private FadeTransition fadeTransition;
+    private AnimationTimer loop;
+    private int bluescore;
+    private int redscore;
+    private double ballYVelocity = 2.5;
+    private double ballXVelocity = 5;
+    //private Random random = new Random();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -41,6 +50,67 @@ public class GamePanelController implements Initializable{
         fadeTransition.setFromValue(0);
         fadeTransition.setToValue(1);
         fadeTransition.play();
+        playerbluescore.setText("Score "+bluescore);
+        playerredscore.setText(redscore+" Score");
+        animation();
+        
+    }
+    
+    private void animation(){
+        loop = new AnimationTimer() {
+            double control = 0;
+            @Override
+            public void handle(long time) {
+                control += 0.15;
+                checkColissions();
+                if(control > 1){
+                    ball.setLayoutY(ball.getLayoutY()+ballYVelocity);
+                    ball.setLayoutX(ball.getLayoutX()+ballXVelocity);
+                    control = 0;
+                }   
+            }
+            
+        };
+        loop.start();
     }
 
+    private void checkColissions(){
+        
+        if(redrectangle.getBoundsInParent().intersects(ball.getBoundsInParent())){
+            ballXVelocity *= -1.01;
+            ballYVelocity *= -1.01;
+            
+
+        } else if(bluerectangle.getBoundsInParent().intersects(ball.getBoundsInParent())){
+            ballXVelocity *= -1.01;
+            ballYVelocity *= -1.01;
+            
+            
+        } else if(ball.getLayoutY() - ball.getRadius() < 0){
+            ballXVelocity *= -1.01;
+            ballYVelocity *= -1.01;
+            
+
+        } else if(ball.getLayoutY() + ball.getRadius() > 500){
+            ballXVelocity *= -1.01;
+            ballYVelocity *= -1.01;
+            
+
+        } if(ball.getLayoutX() + ball.getRadius() > 764){
+            ballXVelocity = 5;
+            ballYVelocity = 3;
+            bluescore++;
+            playerbluescore.setText("Score "+bluescore);
+            ball.setLayoutX(380);
+            ball.setLayoutY(250);
+            
+        } if(ball.getLayoutX() - ball.getRadius() < 0){
+            ballXVelocity = -5;
+            ballYVelocity = 3;
+            redscore++;
+            playerredscore.setText(redscore+" Score");
+            ball.setTranslateX(380);
+            ball.setTranslateY(250);
+        }
+    }
 }
